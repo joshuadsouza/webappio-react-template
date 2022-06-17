50cd2d89a63d17bb2b70b42627d7685240891807
Joshua D'Souza <joshua@Joshuas-MBP.lan>
2022-06-17 08:51:11 -0400

    more text changes

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-        Welcome to webapp.io!
+        Welcome to webapp.io! Glad you're here.
         </p>
         <a
           className="App-link"
